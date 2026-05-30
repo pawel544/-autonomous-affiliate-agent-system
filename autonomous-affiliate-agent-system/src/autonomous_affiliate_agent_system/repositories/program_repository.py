af4daa9aadfb0bc25cbc55994bdf544218cc8903
pawel544 <140202153+pawel544@users.noreply.github.com>
2026-05-30 23:14:49 +0200

    Add files via upload

--- autonomous-affiliate-agent-system/src/autonomous_affiliate_agent_system/repositories/program_repository.py
+++ autonomous-affiliate-agent-system/src/autonomous_affiliate_agent_system/repositories/program_repository.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pathlib import Path
+from typing import Optional
 from ..services.coring_service import mcp
 
 @mcp.tool()
@@ -36,3 +37,36 @@ def list_affiliate_programs() -> list[dict]:
         program = dict(row)
         programs.append(program)
     return {"count": len(programs), "programs": programs}
+
+@mcp.tool()
+def filter_affiliate_programs(id_program:Optional[int]=None, name_program : Optional[str] = None
+                               ,category_program: Optional[str]= None,) -> dict:
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+    DB_PATH = BASE_DIR / 'data' / 'base.db'
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    query ="SELECT * FROM program_affiliate"
+    parms = []
+    if id_program is  not None:
+        query += " WHERE id = ?"
+        parms.append(id_program)
+    elif name_program is not None and category_program is not None:
+        query += " WHERE name LIKE ? AND category LIKE ?"
+        parms.append(f"%{name_program}%")
+        parms.append(f"%{category_program}%")
+    elif name_program is not None:
+        query += " WHERE name LIKE ?"
+        parms.append(f"%{name_program}%")
+    elif category_program is not None:
+        query += " WHERE category LIKE ?"
+        parms.append(f"%{category_program}%")
+    cursor.execute(query, parms)
+    rows = cursor.fetchall()
+    conn.close()
+    programs = []
+    for row in rows:
+        program = dict(row)
+        programs.append(program)
+    return {"count": len(programs), "programs": programs}
+
